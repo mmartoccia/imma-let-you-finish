baf1e809ba7bab229a67591acc284cf4618b2869
Mike Martoccia <michaelmartoccia@c02p71vwg3qp.home>
2015-08-18 13:48:45 -0400

    Make the response true

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ app.post('*', function immaLetYouFinish (req, res) {
 
   // there's a 0.5% chance of interrupting the conversation.
   var probability = Math.random();
-  if (probability <= 0.0005) {
+  if (probability < 1) {
 
     // kanye west's response
     var botResponse = {
